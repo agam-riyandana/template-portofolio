9213c68ee5d1e6578062821b9ee9f295bef9af3b
Agam Riyandana <87102026+agam-riyandana@users.noreply.github.com>
2024-08-19 09:51:07 +0700

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,11 +1,8 @@
 import { motion } from 'framer-motion';
 import {
-  IconBrandWhatsapp,
   IconBrandCampaignmonitor,
   IconBrandInstagram,
-  IconBrandGithub,
   IconBrandLinkedin,
-  IconHeadphones,
 } from '@tabler/icons-react';
 import { SectionMotion } from '../common/motion/Section';
 import { SectionHeader } from '../components/SectionHeader';
@@ -18,38 +15,24 @@ export const Contact = () => {
   const contacts = [
     {
       id: 1,
-      name: 'Whatsapp',
-      image: <IconBrandWhatsapp style={iconStyles} />,
-      description: '+62887676546',
-      link: 'https://wa.me/+62887676546',
+      name: 'Email',
+      image: <IconBrandCampaignmonitor style={iconStyles} />,
+      description: 'admin@agamriyandana.my.id',
+      link: 'mailto:admin@agamriyandana.my.id',
     },
     {
       id: 2,
-      name: 'Email',
-      image: <IconBrandCampaignmonitor style={iconStyles} />,
-      description: 'bayusetiawanipb@gmail.com',
-      link: 'mailto:bayusetiawanipb@gmail.com',
-    },
-    {
-      id: 3,
       name: 'Instagram',
       image: <IconBrandInstagram style={iconStyles} />,
-      description: 'bayustr__',
-      link: 'https://www.instagram.com/bayustr__',
-    },
-    {
-      id: 4,
-      name: 'Github',
-      image: <IconBrandGithub style={iconStyles} />,
-      description: 'Bayusetiawan45',
-      link: 'https://github.com/Bayusetiawan45',
+      description: 'agam_riyandana1',
+      link: 'https://www.instagram.com/agam_riyandana1',
     },
     {
       id: 5,
       name: 'Linkedin',
       image: <IconBrandLinkedin style={iconStyles} />,
-      description: 'bayu-setiawan99',
-      link: 'https://www.linkedin.com/in/bayu-setiawan99/',
+      description: 'agam-riyandana',
+      link: 'https://www.linkedin.com/in/agam-riyandana',
     },
   ];
   return (
