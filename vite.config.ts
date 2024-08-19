6d2cee068aa2f32836cad2139db31675fa8e5580
Agam Riyandana <87102026+agam-riyandana@users.noreply.github.com>
2024-08-19 20:46:53 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
-// vite.config.ts
-import ViteAMP from 'vite-plugin-amp'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ViteAMP from 'vite-plugin-amp'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-export default {
   plugins: [ViteAMP()]
 })
