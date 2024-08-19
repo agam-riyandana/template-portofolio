ccc4ea39eca11bcb4e7495d3394d313eb5bb8cb6
Agam Riyandana <87102026+agam-riyandana@users.noreply.github.com>
2024-08-19 10:00:02 +0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <footer className="text-teal-500 text-center bg-black p-4 border-t border-teal-500 w-screen flex items-center justify-center">
-      <p>CopyRight © 2023 Bayu Setiawan All Rights Reserved</p>
+      <p>CopyRight © {new Date().getFullYear()} Agam Riyandana All Rights Reserved</p>
     </footer>
   );
 };
